dee42c286e6c8cfe9e74fc6a51291d06699b9803
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-10-14 21:20:24 -0300

    Update app.py

--- microblog/app.py
+++ microblog/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask (__name__)
 @app.route('/')
 def index():
-    return '<h2>quero ir embora pra minha casinha dormir<h2>'
+    return 'oieee'
 if __name__ == '__main__':
     app.run()
