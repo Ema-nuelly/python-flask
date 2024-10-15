04159996e0011a2151c330b3e7745bd53af2f3df
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-10-14 21:16:33 -0300

    Update app.py

--- microblog/app.py
+++ microblog/app.py
@@ -1,1 +1,7 @@
-print("getting tried of printing stuff atp")
+from flask import Flask
+app = Flask (__name__)
+@app.route('/')
+def index():
+    return '<h2>quero ir embora pra minha casinha dormir<h2>'
+if __name__ == '__main__':
+    app.run()
