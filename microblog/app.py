0323548a4781d2446978a9092c8afd8e3f5ea31c
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-10-21 19:42:29 -0300

    Update app.py

--- microblog/app.py
+++ microblog/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask (__name__)
 @app.route('/')
 def index():
-    return '<h2>                                                     oieee'
+    return 'oieee'
+
+@app.route("/contato")
+def contato():
+    return '+5587996222499'
 if __name__ == '__main__':
     app.run()
