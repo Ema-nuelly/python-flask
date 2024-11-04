b830acc78bea29914edf95548abb3136b0a038fc
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-11-04 18:30:01 -0300

    Update app.py

--- microblog/app.py
+++ microblog/app.py
@@ -7,8 +7,6 @@ def index():
 @app.route("/contact")
 def contato():
     return render_template("contact.html")
-@app.route("/home")
-def home():
-    return 'HOME SWEET HOME'
+
 if __name__ == '__main__':
     app.run()
