05ff41e63e87911a9eab7a382593877f48a9985e
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-11-04 19:34:07 -0300

    dunno.py

--- microblog/app.py
+++ microblog/app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template
 app = Flask (__name__)
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", hour = "19:06")
 
 @app.route("/contact")
 def contato():
     return render_template("contact.html", phone = "(87) 94002-8922")
+
+@app.route("/biography")
+def bio():
+    return render_template("bio.html", bio = "Conversey")
+
+@app.route("/products")
+def products():
+    return render_template("products.html", disclaimer = "Conversey")
 
 if __name__ == '__main__':
     app.run()
