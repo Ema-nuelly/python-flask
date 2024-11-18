ee9699d75c3a9e723183c206d4e94a510184066b
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2024-11-18 14:55:08 -0300

    segunda18do11
    instalação no meu pc e gpt ajeitou calculadora

--- microblog/app.py
+++ microblog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 app = Flask (__name__)
+app.run(debug=True)
 @app.route('/')
 def index():
     return render_template("index.html", count = "67")
@@ -17,15 +18,20 @@ def products():
     return render_template("products.html", disclaimer = "Please note that at this time, our shoes are available for in-store purchase only. We do not offer delivery services. We appreciate your understanding and encourage you to visit us to try on and purchase your favorite pairs!")
 
 @app.route("/soma/<int:num1>/<int:num2>")
-@app.route("/calculadora/")
-def continha (num1, num2):
+def continha(num1, num2):
     soma = num1 + num2
     subtracao = num1 - num2
     divisao = num1 / num2
     multiplicacao = num1 * num2
-    return f"Soma: {soma}, Subtração: {subtracao}, Divisão: {divisao}, Multiplicação: {multiplicacao}"
+    return render_template(
+        "calc.html",
+        soma=soma,
+        subtracao=subtracao,
+        divisao=divisao,
+        multiplicacao=multiplicacao
+    )
+
 
 if __name__ == '__main__':
     app.run()
 
-@app.route("/minhapagina")
